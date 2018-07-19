51721fe73feb5e129d90d7bec1d5c0ded4a6c870
Michael Nash <michael@spatialnetworks.com>
2018-07-19 16:28:32 -0400

    wrote the grid function

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -18,7 +18,15 @@ defmodule Identicon do
     %Identicon.Image{image | color: {r, g, b}}
   end
 
-  def build_grid do
+  def build_grid(%Identicon.Image{hex: hex}) do
+    hex
+    |> Enum.chunk(3)
+    |> Enum.map(&mirror_row/1)
+  end
 
+  def mirror_row(row) do
+   [first, second | _tail] = row
+
+   row ++ [second, first]
   end
 end
