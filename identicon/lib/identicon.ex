cbf78dc6236ae234e17d9b0b7b32e17194144505
Michael Nash <michael@spatialnetworks.com>
2018-07-19 18:55:54 -0400

    finished identicon

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -7,6 +7,7 @@ defmodule Identicon do
     |> filter_odd_squares
     |> build_pixel_map
     |> draw_image
+    |> save_image(input)
   end
 
   def hash_input(input) do
@@ -65,6 +66,9 @@ defmodule Identicon do
     :egd.render(image)
   end
 
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
+  end
 
   def mirror_row(row) do
    [first, second | _tail] = row
