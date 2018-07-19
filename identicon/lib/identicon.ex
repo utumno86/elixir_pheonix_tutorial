cabd55805cbad8f35d1d1e1524fced0b83d80b0d
Michael Nash <michael@spatialnetworks.com>
2018-07-19 15:03:22 -0400

    pick color function

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -2,10 +2,23 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
+    |> build_grid
   end
 
   def hash_input(input) do
-    :crypto.hash(:md5, input)
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def build_grid do
+
   end
 end
