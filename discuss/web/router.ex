18e101c25301073108775bae72ac399b8969f4f9
Michael Nash <michael@spatialnetworks.com>
2018-07-31 10:43:20 -0400

    resources helper

--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -16,12 +16,7 @@ defmodule Discuss.Router do
   scope "/", Discuss do
     pipe_through :browser # Use the default browser stack
 
-    get "/", TopicController, :index
-    get "/topics/new", TopicController, :new
-    post "/topics", TopicController, :create
-    get "/topics/:id/edit", TopicController, :edit
-    put "/topics/:id", TopicController, :update
-    get "/topics/:id", TopicController, :show
+    resources "/", TopicController
   end
 
   # Other scopes may use custom stacks.
