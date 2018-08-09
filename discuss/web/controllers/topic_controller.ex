a755000d11a6732a1ebc38829c9bec9503a312b0
Michael Nash <michael@spatialnetworks.com>
2018-08-09 16:31:44 -0400

    a user can only edit a topic that he created

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -5,6 +5,8 @@ defmodule Discuss.TopicController do
 
   plug Discuss.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete]
 
+  plug :check_topic_owner when action in [:update, :edit, :delete]
+
   def index(conn, _params) do
     IO.inspect(conn.assigns)
     topics = Repo.all(Topic)
@@ -61,5 +63,18 @@ defmodule Discuss.TopicController do
     conn
     |> put_flash(:info, "Topic Deleted")
     |> redirect(to: topic_path(conn, :index))
+  end
+
+  def check_topic_owner(conn, _params) do
+    %{params: %{"id" => topic_id}} = conn
+
+    if Repo.get(Topic, topic_id).user_id == conn.assigns.user.id do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You cannot edit that")
+      |> redirect(to: topic_path(conn, :index))
+      |> halt()
+    end
   end
 end
