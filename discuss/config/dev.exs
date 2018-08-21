eeeb1af9d827a9158d091dbb076d115899d445c4
Michael Nash <michael@spatialnetworks.com>
2018-08-21 16:44:52 -0400

    some modifications

--- discuss/config/dev.exs
+++ discuss/config/dev.exs
@@ -49,7 +49,7 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :discuss, Discuss.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "michael",
+  username: "postgres",
   password: "",
   database: "discuss_dev",
   hostname: "localhost",
