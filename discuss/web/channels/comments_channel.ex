52fab8f4e0ad7b095298bd715dc06fdac30694c7
Michael Nash <michael@spatialnetworks.com>
2018-08-22 12:01:52 -0400

    installed eslint

--- discuss/web/channels/comments_channel.ex
+++ discuss/web/channels/comments_channel.ex
@@ -2,12 +2,12 @@ defmodule Discuss.CommentsChannel do
   use Discuss.Web, :channel
 
   def join(name, _params, socket) do
-    IO.puts("+++++++++")
-    IO.puts name
     {:ok, %{hey: "there"}, socket}
   end
 
-  def handle_in() do
-    
+  def handle_in(name) do
+    IO.puts("++++++")
+    IO.puts(name)
+    IO.inspect(message)
   end
 end
